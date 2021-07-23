1650aae8a4ff6d85fbff6e6205ee5e32b91dd62a
Nataliia <natalija.sirobaba@gmail.com>
2021-07-24 02:36:44 +0300

    Put apk keys to local.properties

--- app/src/main/java/com/zarudna/foodplacesnearby/MyApplication.kt
+++ app/src/main/java/com/zarudna/foodplacesnearby/MyApplication.kt
@@ -1,18 +1,23 @@
 package com.zarudna.foodplacesnearby
 
 import android.app.Application
+import android.content.pm.PackageManager
 import com.esri.arcgisruntime.ArcGISRuntimeEnvironment
+import com.zarudna.foodplacesnearby.injections.AppComponent
 import com.zarudna.foodplacesnearby.injections.DaggerAppComponent
 import io.realm.Realm
 
 class MyApplication : Application() {
 
-    val appComponent = DaggerAppComponent.create()
+    val appComponent: AppComponent = DaggerAppComponent.create()
 
     override fun onCreate() {
         super.onCreate()
 
-        ArcGISRuntimeEnvironment.setApiKey(getString(R.string.arc_gis_key));
+        val metaData =
+            packageManager.getApplicationInfo(packageName, PackageManager.GET_META_DATA).metaData
+        ArcGISRuntimeEnvironment.setApiKey(metaData.getString("arc_gis.API_KEY"))
+
         Realm.init(this)
     }
 }
